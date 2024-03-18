c7edde2082ddac847868a1765cfd9015bad2ef20
thailinoo <thaila.lino45@gmail.com>
2024-03-18 08:26:28 -0300

    Formulário

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
 
+var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var lograudoro = document.getElementById("lograudoro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
+
 function alertar() {
-    alert("O botão foi clicado!!!");
+    //alert("O nome inserido foi " + nome.value);
+
+    saida.innerText = "Nome:" + nome.value + 
+           "\n E-mail:" + email.value + 
+           "\n Telefone:" + telefone.value +
+           "\n CEP:" + cep.value +
+           "\n Lograudoro:" + lograudoro.value +
+           "\n Número:" + numero.value +
+           "\n Complemento:" + complemento.value +
+           "\n Bairro:" + bairro.value +
+           "\n Cidade:" + cidade.value +
+           "\n Estado:" + estado.value 
+
 }
